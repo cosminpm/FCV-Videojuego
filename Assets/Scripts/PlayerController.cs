438329d7e01e85aab4342217285e8e6070586383
Cosmin <cosminmarian.paduraru@gmail.com>
2021-11-24 23:14:06 +0100

    Box collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed, dashDistance;
     private Vector3 _lastMoveDir;
-
+    public AudioSource dashSound;
     private void Update()
     {
         HandleMovement();
@@ -43,17 +43,53 @@ public class PlayerController : MonoBehaviour
         bool isIdle = moveX == 0 && moveZ == 0;
         if (!isIdle)
         {
-            // Normalize to not move faster diagonal
             Vector3 moveDir = new Vector3(moveX, 0, moveZ).normalized;
-            _lastMoveDir = moveDir;
-            transform.position += moveDir * speed * Time.deltaTime; 
+            TryMove(moveDir, speed * Time.deltaTime);
         }
     }
 
+    private bool CanMove(Vector3 dir, float distance)
+    {
+        return  !Physics.Raycast(transform.position, dir, distance);
+    }
+
+    private bool TryMove(Vector3 baseMoveDir, float distance)
+    {
+        Vector3 moveDir = baseMoveDir;
+        bool canMove = CanMove(moveDir, distance);
+        if (!canMove)
+        {
+            // Can't move diagonally
+            moveDir = new Vector3(baseMoveDir.x, 0f,0f).normalized;
+            canMove = moveDir.x != 0 && CanMove(moveDir, distance);
+            if (!canMove)
+            {
+                // Cannot move horizontally
+                moveDir = new Vector3(0f, 0f,baseMoveDir.z).normalized;
+                canMove = moveDir.z != 0f && CanMove(moveDir, distance);
+            }
+        }
+        if (canMove)
+        {
+            _lastMoveDir = moveDir;
+            transform.position += moveDir * distance;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    
     private void HandleDash()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            if (!dashSound.isPlaying)
+            {
+                dashSound.Play();
+            }
             transform.position += _lastMoveDir * dashDistance;
         }
     }
